ffdd8112f8884fcf2142f6f743cc2686090d88e6
Omadoye Jedidiah <omadoyejed@gmail.com>
2017-02-08 23:17:30 +0100

    made getPrimes function more robust

--- getPrimes.js
+++ getPrimes.js
@@ -2,13 +2,17 @@ function getPrimes(num){
   var newArray = [];
   var e = 0;
 	for (i = 2; i<=num; i++){
-		if (i % 2 === 0 & i !== 2){e = 1;}
-	    else if (i % 3 === 0 & i !== 3){e = 1;}
-	    else if (i % 5 === 0 & i !== 5){e = 1;}
-	    else if (i % 7 === 0 & i !== 7){e = 1;}
+		if (i % 2 === 0 & i !== 2){continue;}
+	    else if (i % 3 === 0 & i !== 3){continue;}
+	    else if (i % 5 === 0 & i !== 5){continue;}
+	    else if (i % 7 === 0 & i !== 7){continue;}
+	    else if (i % 11 === 0 & i !== 11){continue;}
+	    else if (i % 13 === 0 & i !== 13){continue;}
+	    else if (i % 17 === 0 & i !== 17){continue;}
+	    else if (i % 19 === 0 & i !== 19){continue;}
 	    else{newArray.push(i);}
 		
 	}
 	return newArray;
 }
-getPrimes(500);
+getPrimes(100);
