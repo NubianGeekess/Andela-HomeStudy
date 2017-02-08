c99549996a76e757ab12a8c78117465e5cf099da
Omadoye Jedidiah <omadoyejed@gmail.com>
2017-02-08 22:58:20 +0100

    working getPrimes function | needs refactoring

--- getPrimes.js
+++ getPrimes.js
@@ -1,10 +1,14 @@
-function getPrimes(){
-	for (i = 2; i<100; i++){
-		if (i % 2 === 0 & i !== 2){document.write();}
-	    else if (i % 3 === 0 & i !== 3){document.write();}
-	    else if (i % 5 === 0 & i !== 5){document.write();}
-	    else if (i % 7 === 0 & i !== 7){document.write();}
-	    else{document.write(i + " ");}
+function getPrimes(num){
+  var newArray = [];
+  var e = 0;
+	for (i = 2; i<=num; i++){
+		if (i % 2 === 0 & i !== 2){e = 1;}
+	    else if (i % 3 === 0 & i !== 3){e = 1;}
+	    else if (i % 5 === 0 & i !== 5){e = 1;}
+	    else if (i % 7 === 0 & i !== 7){e = 1;}
+	    else{newArray.push(i);}
 		
 	}
+	return newArray;
 }
+getPrimes(500);
