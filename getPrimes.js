17bbe49b53598210ab48b404ee800a71f225df12
Omadoye Jedidiah <omadoyejed@gmail.com>
2017-02-08 22:43:59 +0100

    first commit

---
+++ getPrimes.js
@@ -0,0 +1,10 @@
+function getPrimes(){
+	for (i = 2; i<100; i++){
+		if (i % 2 === 0 & i !== 2){document.write();}
+	    else if (i % 3 === 0 & i !== 3){document.write();}
+	    else if (i % 5 === 0 & i !== 5){document.write();}
+	    else if (i % 7 === 0 & i !== 7){document.write();}
+	    else{document.write(i + " ");}
+		
+	}
+}
