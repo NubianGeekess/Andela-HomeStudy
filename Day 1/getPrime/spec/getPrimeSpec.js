2fe6eed7e8295fa9574ccbdb5ab22864eb471e6c
Omadoye Jedidiah <omadoyejed@gmail.com>
2017-02-12 15:58:44 +0100

    added more tests

--- Day 1/getPrime/spec/getPrimeSpec.js
+++ Day 1/getPrime/spec/getPrimeSpec.js
@@ -3,7 +3,6 @@ var myApp = require('../src/getPrimes.js');
   'use strict';
 
     describe("Case for an arithmetic sequence", function() {
-
       it("should return `[2, 3, 5, 7, 11, 13, 17, 19,23]` for getPrimes(23)", function() {
         expect(myApp.getPrimes(23)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23]);
       });
