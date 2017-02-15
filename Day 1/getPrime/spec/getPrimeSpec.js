0dddd91722526bace2afb795dd0a54b96e5a7146
Omadoye Jedidiah <omadoyejed@gmail.com>
2017-02-15 02:34:32 +0100

    Changed to Airbnb Format

--- Day 1/getPrime/spec/getPrimeSpec.js
+++ Day 1/getPrime/spec/getPrimeSpec.js
@@ -1,97 +1,97 @@
 var myApp = require('../src/getPrimes.js');
-(function(){
+(function () {
   'use strict';
 
-    describe("Case for an arithmetic sequence", function() {
-      it("should return `[2, 3, 5, 7, 11, 13, 17, 19,23]` for getPrimes(23)", function() {
-        expect(myApp.getPrimes(23)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23]);
-      });
-
-      it("should return `[2]` for getPrimes(2)", function() {
-        expect(myApp.getPrimes(2)).toEqual([2]);
-      });
-
-      it("should return `[2,3]` for getPrimes(3)", function() {
-        expect(myApp.getPrimes(3)).toEqual([2,3]);
-      });
-
-      it("should return `[2, 3, 5, 7,11]` for getPrimes(11)", function() {
-        expect(myApp.getPrimes(11)).toEqual([2, 3, 5, 7, 11]);
-      });
-
-      it("should return `[2, 3, 5, 7, 11, 13, 17, 19,23]` for getPrimes(23)", function() {
-        expect(myApp.getPrimes(23)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23]);
-      });
-
-      it("should return `[2, 3, 5, 7, 11, 13, 17, 19]` for getPrimes(19)", function() {
-        expect(myApp.getPrimes(19)).toEqual([2, 3, 5, 7, 11, 13, 17, 19]);
-      });
-
-      it("should return `Your max number must be greater than 1` for getPrimes(1)", function() {
-        expect(myApp.getPrimes(1)).toEqual("Your max number must be greater than 1");
-      });
-
-      it("should return `[2,3,  5,7, 11, 13, 17, 19, 23, 29,31, 37, 41, 43, 47,53, 59, 61, 67, 71, 73, 79, 83, 89, 97]` for getPrimes(23)", function() {
-        expect(myApp.getPrimes(100)).toEqual([2,3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]);
-      });
-
+  describe("Case for an arithmetic sequence", function () {
+    it("should return `[2, 3, 5, 7, 11, 13, 17, 19,23]` for getPrimes(23)", function () {
+      expect(myApp.getPrimes(23)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23]);
     });
 
-    describe("Case of invalid datatype",function(){
-
-      it("It should be an integer", function(){
-        expect(myApp.getPrimes("hello")).toEqual("your data type must be a number");
-      });
-
-      it("It should be an integer", function(){
-        expect(myApp.getPrimes("2")).toEqual("your data type must be a number");
-      });
-
-      it("It should be an integer", function(){
-        expect(myApp.getPrimes([1,2,3,4])).toEqual("your data type must be a number");
-      });
-
-      it("It should be an integer", function(){
-        expect(myApp.getPrimes({})).toEqual("your data type must be a number");
-      });
-
-      it("It should be an integer", function(){
-        expect(myApp.getPrimes(undefined)).toEqual("your data type must be a number");
-      });
-
-      it("It should be an integer", function(){
-        expect(myApp.getPrimes(null)).toEqual("your data type must be a number");
-      });
-     
+    it("should return `[2]` for getPrimes(2)", function () {
+      expect(myApp.getPrimes(2)).toEqual([2]);
     });
 
-    describe("Case of max < 2", function(){
-
-      it("It should be an greater than 1", function(){
-        expect(myApp.getPrimes(1)).toEqual("Your max number must be greater than 1");
-      });
-
-      it("It should be an greater than 1", function(){
-        expect(myApp.getPrimes(0)).toEqual("Your max number must be greater than 1");
-      });
-
-      it("It should be an greater than 1", function(){
-        expect(myApp.getPrimes(1.999)).toEqual("Your max number must be greater than 1");
-      });
-
-      it("It should be an greater than 1", function(){
-        expect(myApp.getPrimes(-2)).toEqual("Your max number must be greater than 1");
-      });
-
-      it("It should be an greater than 1", function(){
-        expect(myApp.getPrimes(-99999999999)).toEqual("Your max number must be greater than 1");
-      });
-
-      it("It should be an greater than 1", function(){
-        expect(myApp.getPrimes(-0)).toEqual("Your max number must be greater than 1");
-      });
-
+    it("should return `[2,3]` for getPrimes(3)", function () {
+      expect(myApp.getPrimes(3)).toEqual([2, 3]);
     });
+
+    it("should return `[2, 3, 5, 7,11]` for getPrimes(11)", function () {
+      expect(myApp.getPrimes(11)).toEqual([2, 3, 5, 7, 11]);
+    });
+
+    it("should return `[2, 3, 5, 7, 11, 13, 17, 19,23]` for getPrimes(23)", function () {
+      expect(myApp.getPrimes(23)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23]);
+    });
+
+    it("should return `[2, 3, 5, 7, 11, 13, 17, 19]` for getPrimes(19)", function () {
+      expect(myApp.getPrimes(19)).toEqual([2, 3, 5, 7, 11, 13, 17, 19]);
+    });
+
+    it("should return `Your max number must be greater than 1` for getPrimes(1)", function () {
+      expect(myApp.getPrimes(1)).toEqual("Your max number must be greater than 1");
+    });
+
+    it("should return `[2,3,  5,7, 11, 13, 17, 19, 23, 29,31, 37, 41, 43, 47,53, 59, 61, 67, 71, 73, 79, 83, 89, 97]` for getPrimes(23)", function () {
+      expect(myApp.getPrimes(100)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]);
+    });
+
+  });
+
+  describe("Case of invalid datatype", function () {
+
+    it("It should be an integer", function () {
+      expect(myApp.getPrimes("hello")).toEqual("your data type must be a number");
+    });
+
+    it("It should be an integer", function () {
+      expect(myApp.getPrimes("2")).toEqual("your data type must be a number");
+    });
+
+    it("It should be an integer", function () {
+      expect(myApp.getPrimes([1, 2, 3, 4])).toEqual("your data type must be a number");
+    });
+
+    it("It should be an integer", function () {
+      expect(myApp.getPrimes({})).toEqual("your data type must be a number");
+    });
+
+    it("It should be an integer", function () {
+      expect(myApp.getPrimes(undefined)).toEqual("your data type must be a number");
+    });
+
+    it("It should be an integer", function () {
+      expect(myApp.getPrimes(null)).toEqual("your data type must be a number");
+    });
+
+  });
+
+  describe("Case of max < 2", function () {
+
+    it("It should be an greater than 1", function () {
+      expect(myApp.getPrimes(1)).toEqual("Your max number must be greater than 1");
+    });
+
+    it("It should be an greater than 1", function () {
+      expect(myApp.getPrimes(0)).toEqual("Your max number must be greater than 1");
+    });
+
+    it("It should be an greater than 1", function () {
+      expect(myApp.getPrimes(1.999)).toEqual("Your max number must be greater than 1");
+    });
+
+    it("It should be an greater than 1", function () {
+      expect(myApp.getPrimes(-2)).toEqual("Your max number must be greater than 1");
+    });
+
+    it("It should be an greater than 1", function () {
+      expect(myApp.getPrimes(-99999999999)).toEqual("Your max number must be greater than 1");
+    });
+
+    it("It should be an greater than 1", function () {
+      expect(myApp.getPrimes(-0)).toEqual("Your max number must be greater than 1");
+    });
+
+  });
 
 
 
--- Day 1/getPrime/src/getPrimes.js
+++ Day 1/getPrime/src/getPrimes.js
@@ -8,46 +8,44 @@ Your code should also address edge cases (invalid input, negative input value, o
 */
 var myApp = {
 
-	getPrimes: function (max){
-	  
-	  		//*******USING SEIVE OF ERATOSTHENES*******
-	    if(typeof(max) === 'number' && max >= 2){
-		    var arr = [];
-		    var newArray = [];
-		    var answer;
+  getPrimes: function (max) {
 
-		    // Set all elements between 0 to max in the array to true 
-		    for (var i = 0; i <= max; i++) {
-		        arr.push(true);
-		    }
+    //*******USING SEIVE OF ERATOSTHENES*******
+    if (typeof (max) === 'number' && max >= 2) {
+      var arr = [];
+      var newArray = [];
+      var answer;
 
-		    // Sieve the Two's and Sieve the Three's, The Sieve of Eratosthenes.
-			//When the multiples sublime,The numbers that remain are Prime.
-			//The best way to explain this for loop is the poem above.
-		    for (var i = 2; i <= Math.sqrt(max); i++) {
-		        if (arr[i]) {
-		            for (var j = i * i; j <= max; j += i) {
-		                arr[j] = false;
-		            }
-		        }
-		    }
-		    
-		    //For every true element in the array push the index value to a new array 
-		    //E.g [true,false,true] == [0,2], Only th true indexes are pushed
-		    for (var i = 2; i <= max; i++) {
-		        if(arr[i]) {
-		            newArray.push(i);
-		        }
-		    }
+      // Set all elements between 0 to max in the array to true 
+      for (var i = 0; i <= max; i++) {
+        arr.push(true);
+      }
 
-		    answer =  newArray;
-	    }else if (typeof(max) === 'number' && max < 2) {
-	    	answer = "Your max number must be greater than 1"
-	    }else{
-	    	answer = "your data type must be a number"
-	    }
-	    return answer;
-	}
+      // Sieve the Two's and Sieve the Three's, The Sieve of Eratosthenes.
+      //When the multiples sublime,The numbers that remain are Prime.
+      //The best way to explain this for loop is the poem above.
+      for (var i = 2; i <= Math.sqrt(max); i++) {
+        for (var j = i * i; j <= max; j += i) {
+          arr[j] = false;
+        }
+      }
+
+      //For every true element in the array push the index value to a new array 
+      //E.g [true,false,true] == [0,2], Only th true indexes are pushed
+      for (var i = 2; i <= max; i++) {
+        //if (arr[i]) {
+          newArray.push(i);
+        //}
+      }
+
+      answer = newArray;
+    } else if (typeof (max) === 'number' && max < 2) {
+      answer = "Your max number must be greater than 1"
+    } else {
+      answer = "your data type must be a number"
+    }
+    return answer;
+  }
 }
 
 module.exports = myApp;
