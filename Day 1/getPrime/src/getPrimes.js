a98b8a42a51eae155f34f4228023c8cdc87cd538
Omadoye Jedidiah <omadoyejed@gmail.com>
2017-02-15 13:35:00 +0100

    Refactored code

--- Day 1/getPrime/src/getPrimes.js
+++ Day 1/getPrime/src/getPrimes.js
@@ -9,28 +9,35 @@ Your code should also address edge cases (invalid input, negative input value, o
 var myApp = {
 
   getPrimes: function (max) {
-
     //*******USING SEIVE OF ERATOSTHENES*******
     var answer;
+    //Ensure input value is valid
     if (typeof (max) === 'number' && max >= 2) {
+      //Function that sieves throught an array of numbers
       var sieve = function (list, n) {
+        //Set the number to start sieving the array from
+        //2 is the default sieve starting value
         if (!n) n = 2;
         var listB = [];
+        //Loop through the array and remove factors of n
         list.forEach(function (i) {
           if (i === n || i % n) {
             listB.push(i);
           }
         });
         var y;
+        //set y to the next bigger value after n
         for (var i = 0; i < listB.length; i++) {
           if (listB[i] > n) {
             y = listB[i];
             break;
           }
         }
+        //if the final array if y is true
         if (!y) {
           return listB;
         }
+        //if y is false sieve through the array again with a bigger value of y
         return sieve(listB, y);
       };
       var arr = [];
